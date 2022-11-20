2a58b1a611ab9e6a875769f8e2d44f9906f405b2
George Song <george@damacy.net>
2022-11-19 16:24:14 -0800

    feat: add script to create the shadow db after the fact

---
+++ postgres-prisma/create-shadow-db.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<- EOSQL
+  SELECT 'CREATE DATABASE ${SHADOW_DB}' WHERE NOT EXISTS
+  (SELECT FROM pg_database WHERE datname = '${SHADOW_DB}')\gexec
+	GRANT ALL PRIVILEGES ON DATABASE ${SHADOW_DB} TO ${POSTGRES_USER};
+EOSQL
